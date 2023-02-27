79af47daae8bb8c5307c23b10e3159f460881d7e
ilike3p <39431924+ilike3p@users.noreply.github.com>
2023-02-26 21:50:07 -0800

    update css

---
+++ seeds/user.js
@@ -0,0 +1,22 @@
+const { User } = require("../models");
+
+const userData = [
+  {
+    username: "trayp",
+    password: "pass1234",
+  },
+  {
+    username: "Future",
+    password: "pass1234",
+  },
+  {
+    username: "Girls",
+    password: "pass1234",
+  },
+];
+
+const seedUsers = () => User.bulkCreate(userData);
+
+// The password will not be hashed by seed bulk create. It has to be hashed via the update route before the login route
+// sign up new and use the new login is recommended.
+module.exports = seedUsers;
